99e7b0daf59b8d22aa8cc86cf323aba59bd6b818
gupranay <gupranay@umich.edu>
2024-10-30 17:14:22 -0400

    please

--- components/actively-page.tsx
+++ components/actively-page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { X } from 'lucide-react';
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
--- components/app-page.tsx
+++ components/app-page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState, useEffect } from 'react'
 import { FileText, Github, Linkedin } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
